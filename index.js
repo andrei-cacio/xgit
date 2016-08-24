78f83d12b27879b6d6c6a43ebf2110ebdf88edba
Andrei Cacio <andrei.cacio@yahoo.com>
2016-08-24 23:38:09 +0300

    Added branch name in upper box

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const repo = require('nodegit').Repository;
 const ref = require('nodegit').Reference;
 const blessed = require('blessed');
+const help = require('./howto');
+const repoPath = process.argv[2];
 
 const screen = blessed.screen({ smartCSR: true });
 screen.title = 'xGit';
 
 const style = {
+	top: 'center',
+	border: {
+		bg: 'white'
+	},
 	selected: {
 		fg: 'blue'
 	},
@@ -14,27 +20,89 @@ const style = {
 	}
 }
 
+const statusBox = blessed.box({
+	top: 'top',
+	left: 'center',
+	width: '100%',
+	height: '12%',
+	border: {
+	    type: 'line'
+	  },
+	  style: {
+	    fg: 'white',
 
+	    border: {
+	      fg: '#f0f0f0'
+	    },
+	  }
+});
+
+
+
+const box = blessed.box({
+	top: 'center',
+	left: 'left',
+	width: '50%',
+	height: '80%',
+
+	border: {
+	    type: 'line'
+	  },
+	  style: {
+	    fg: 'white',
+	    bg: 'blue',
+	    border: {
+	      fg: '#f0f0f0'
+	    },
+	    hover: {
+	      bg: 'green'
+	    }
+	  }
+});
+
+const box2 = blessed.box({
+	top: 'center',
+	left: '50%',
+	width: '50%',
+	height: '80%',
+	content: help,
+	border: {
+	    type: 'line'
+	  },
+	  style: {
+	    fg: 'white',
+	    
+	    border: {
+	      fg: '#f0f0f0'
+	    }
+	  }
+});
 
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
   return process.exit(0);
 });
 
-screen.render();
+// screen.render();
 
-repo.open('../Calendis-Client')
-	.then(rep => {
+repo.open(repoPath)
+	.then(rep => {		
+		rep.getCurrentBranch().then(displayBranch);
 		rep.getStatus()
 			.then(display);
+		});
+		
 
-	})
-
-
+function displayBranch(ref) {
+	statusBox.setContent(`Current branch: ${ref.name()}]`);
+	screen.append(statusBox);
+}
 function display(statuses) {
 	const colored = statuses.map(status => status.path());
-	const list = blessed.list({items: colored, keys: true, style: style, vi: true});
+	const list = blessed.list({items: colored, keys: true, style: style, vi: true, parent: box});
 	list.focus();
-	screen.append(list);
+	screen.append(statusBox);
+	screen.append(box);
+	screen.append(box2);
 	screen.render();
 }
 
